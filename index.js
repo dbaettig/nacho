3ff93905b3a0184327289e22b8b338735cd6eda8
dbaettig <daniela.baettig@hotmail.com>
2018-09-28 14:12:09 +0200

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function scrollTo(element) {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: element.offsetTop
+    });
+}
+
+document.getElementById("scrollArrow").addEventListener('click', () => {
+    scrollTo(document.getElementById("text"));
+});  
